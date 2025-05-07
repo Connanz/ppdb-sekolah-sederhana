2f82e9e1f6a8f0d0e66dbebb29432144e600e988
connanz <rickvalcao@gmail.com>
2025-05-07 09:45:09 +0700

    - Add login and registration templates with Tailwind CSS styling
    - Created login.html for user authentication with form handling and flash messages.
    
    - Created register.html for user registration with form handling and flash messages.
    
    - Added contact.html template as a placeholder.
    
    - Added backup.html containing the old version of index.html for reference.
    
    - Delete database.py because replaced by models.py in app folder

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,8 +1,9 @@
 from flask import render_template, Blueprint
 from flask_login import login_required, current_user
 
-index_bp = Blueprint('index', __name__)
+index_bp = Blueprint('index_bp', __name__)
 
 @index_bp.route('/')
+@login_required
 def index():
     return render_template('index.html')
