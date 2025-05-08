0e3dbf7c876c954e22adf46e593348e34903cf99
connanz <rickvalcao@gmail.com>
2025-05-08 13:32:17 +0700

    Fixing register and login and add logout option

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -58,3 +58,9 @@ def register():
 
     return render_template('auth/register.html')
 
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index_bp.index'))
+
